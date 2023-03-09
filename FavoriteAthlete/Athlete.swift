9db18c93893b56bbd95f4dfaea44a9fa27e9ce52
Colin Jones <colinjones0123@gmail.com>
2023-03-09 08:58:20 -0700

    Initial Commit

---
+++ FavoriteAthlete/Athlete.swift
@@ -0,0 +1,18 @@
+//
+//  Athlete.swift
+//  FavoriteAthlete
+//
+//  Created by Colin Jones on 3/3/23.
+//
+
+import Foundation
+
+struct Athlete {
+    var name: String
+    var age: Int
+    var league: String
+    var team: String
+    var description: String {
+        return "\(name) is \(age) years old and plays for the \(team) in the \(league)."
+    }
+}
